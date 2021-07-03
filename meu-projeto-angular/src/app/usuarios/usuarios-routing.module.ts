6b8ec5c0e199a2f2b3b15b51ad8ed0c2d9a551b9
Yago Ananias <yago_souza92@yahoo.com.br>
2021-07-03 06:34:19 -0300

    Ajustes nas rotas

--- meu-projeto-angular/src/app/usuarios/usuarios-routing.module.ts
+++ meu-projeto-angular/src/app/usuarios/usuarios-routing.module.ts
@@ -1,9 +1,10 @@
+import { UsuarioListComponent } from './usuario-list/usuario-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioDetalheComponent } from './usuario-detalhe/usuario-detalhe.component';
 
 const routes: Routes = [
-  { path:'usuarios', component: UsuarioDetalheComponent }
+  { path:'usuarios', component: UsuarioListComponent }
 ];
 
 @NgModule({
